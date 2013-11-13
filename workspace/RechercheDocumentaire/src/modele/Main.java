75f55a59f8f24d2ef947416117a120ca08a0cdee
geeknbar <dorian.coffinet@gmail.com>
2013-11-13 09:32:55 +0100

    lien entre le lecteur de fichier et la partie parser,stemmer,dic

--- workspace/RechercheDocumentaire/src/modele/Main.java
+++ workspace/RechercheDocumentaire/src/modele/Main.java
@@ -1,18 +1,26 @@
 package modele;
 
+import java.util.ArrayList;
+
 public class Main {
 
 	public static void main(String[] args) {
-		Parser p = new Parser();
-		p.init("./src/doc/AP890101.txt");
-		Stemmer s = new Stemmer();
-		//permet de comparer les deux méthodes entre celle qui est donner par le mec et la notre
-		//il reste un souci avec les mots composés
-		s.init();
-		s.stemmerArray(p.getLines());
-		Dictionary dic = new Dictionary();
-		int docID = 890101;
-		dic.fillDictionary(s.getStemmerFile2(),docID);
-		dic.displayInfos();
+		String path = "~/Documents/";
+		DirectoryBrowsing direct = new DirectoryBrowsing(path);
+		ArrayList<String> listCorpus = direct.getFilesPath();
+		for (String filePath : listCorpus) {
+			Parser p = new Parser();
+			p.init(filePath);
+			Stemmer s = new Stemmer();
+			//permet de comparer les deux méthodes entre celle qui est donner par le mec et la notre
+			//il reste un souci avec les mots composés
+			s.init();
+			s.stemmerArray(p.getLines());
+			Dictionary dic = new Dictionary();
+			int docID = 890101;
+			dic.fillDictionary(s.getStemmerFile2(),docID);
+			dic.displayInfos();
+		}
+		
 	}
 }
